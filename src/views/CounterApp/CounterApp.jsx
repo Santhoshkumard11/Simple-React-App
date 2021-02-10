c9d5d89b0b4a2dd4441bf3aff0cf68fb65dbfe41
SANTHOSHKUMARD <31656855+Santhoshkumard11@users.noreply.github.com>
2021-02-10 11:26:09 +0530

    T003-004 added icons

--- src/views/CounterApp/CounterApp.jsx
+++ src/views/CounterApp/CounterApp.jsx
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 //ui components
 import Button from "@material-ui/core/Button";
 import { Grid, GridListTileBar } from "@material-ui/core";
-
+import Icon from "@material-ui/core/Icon";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import CloseIcon from "@material-ui/icons/Close";
 
 export default class Counter extends Component {
   constructor(props) {
@@ -21,19 +23,29 @@ export default class Counter extends Component {
     });
   };
 
-    
+  
   render() {
     return (
       <Grid container direction="row" justify="center" alignItems="center">
         <GridListTileBar> Counter App - Mall</GridListTileBar>
         <Grid item xs={6} sm={3}>
-          <Button variant="outlined" color="primary" onClick={this.updateEntry}>
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={this.updateEntry}
+            startIcon={<AddCircleIcon />}
+          >
             Login
           </Button>
           <h3>{this.state.entryCount} People entered!! </h3>
         </Grid>
         <Grid item xs={6} sm={3}>
-                <Button variant="outlined" color="secondary" onClick={this.updateExit}>
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={this.updateExit}
+            startIcon={<CloseIcon />}
+          >
             Exit
           </Button>
           <h3>{this.state.exitCount} People Left!! </h3>
@@ -42,9 +54,3 @@ export default class Counter extends Component {
     );
   }
 }
-
-function gitShorthand($m = "initial commit") {
-    git add .
-    git commit -m $m
-    git push -u origin master
- }
